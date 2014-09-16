05000d2665c1f447f25a75d933c7014a50a079a7
David Rueck <drueck@gmail.com>
2014-09-15 21:31:50 -0700

    Allow users to add passages and select a passage to practice
    Stores the passages in localStorage using a really naive approach.

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 $(function() {
 
+  updatePassagesList();
+
+  $(document).on("click", "#add-passage-button", function(e) {
+    e.preventDefault();
+    $("#passages").hide();
+    $("#practice-passage-button").hide();
+    $("#add-passage-button").hide();
+    $("#add-passage-form").show();
+    $("#save-passage-button").show();
+  });
+
+  $(document).on("click", "#save-passage-button", function(e) {
+    e.preventDefault();
+    var passage = {};
+    passage.reference = $("#new-passage-reference").val();
+    passage.text = $("#new-passage-text").val();
+    storePassage(passage);
+    updatePassagesList();
+    $("#add-passage-form").hide();
+    $("#passages").show();
+    $("#practice-passage-button").show();
+    $("#add-passage-button").show();
+  });
+
+  $(document).on("click", "#practice-passage-button", function(e) {
+    var reference = $("#passages").val();
+    var passage = getPassageByReference(reference);
+    practicePassage(passage);
+  });
+
   $(document).on("click", "#toggle-source", function(e) {
     e.preventDefault();
     var toggleSource = $("#toggle-source");
@@ -33,3 +63,37 @@ $(function() {
   });
 
 });
+
+function updatePassagesList() {
+  var passagesSelect = $("#passages");
+  passagesSelect.empty();
+  var option;
+  var passages = getPassages();
+  passages.forEach(function(passage) {
+    option = '<option value="' + passage.reference + '">' + passage.reference + '</option>';
+    passagesSelect.append(option);
+  });
+}
+
+function practicePassage(passage) {
+  $("#manage-passages").hide();
+  $("#source").text(passage.text);
+  $("#practice-form").show();
+  $("#practice").focus();
+}
+
+function getPassageByReference(reference) {
+  return _.find(getPassages(), function(passage) {
+    return passage.reference === reference;
+  });
+}
+
+function storePassage(passage) {
+  var passages = JSON.parse(localStorage.getItem("passages")) || [];
+  passages.push(passage);
+  localStorage.setItem("passages", JSON.stringify(passages));
+}
+
+function getPassages() {
+  return JSON.parse(localStorage.getItem("passages"));
+}
